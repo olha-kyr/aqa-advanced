3007c87ab8dd3a319a46629ea3b5e4b8833ddeea
Olha Kyrychenko <olhakyrychenko21@gmail.com>
2024-10-24 22:17:47 +0300

    Lesson 7 commit

---
+++ lesson7/task3.js
@@ -0,0 +1,33 @@
+function divide(numerator, denominator) {
+  function isNotNumber(num) {
+    return typeof num !== "number";
+  }
+
+  if (denominator === 0) {
+    throw Error("Denominator equals 0");
+  }
+  if (isNotNumber(numerator) || isNotNumber(denominator)) {
+    throw Error("Numerator or denomerator is not a number");
+  }
+  return numerator / denominator;
+}
+
+let result = divide(3, 4);
+
+try {
+  divide(7, 0);
+} catch (error) {
+  console.error("Dividing by 0 check error:", error.message);
+} finally {
+  console.log("Job's done!");
+}
+
+try {
+  divide("test", 2);
+} catch (error) {
+  console.error("Non-number check error:", error.message);
+} finally {
+  console.log("Job's done!");
+}
+
+console.log(result);
